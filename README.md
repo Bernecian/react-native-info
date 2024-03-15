c802425fed0b7596d60b2503f1ac174642202212
Bernecian <bernecyan.dev@gmail.com>
2024-03-16 00:28:09 +0400

    Init

---
+++ README.md
@@ -0,0 +1,31 @@
+# react-native-info
+
+Get metadata info
+
+## Installation
+
+```sh
+npm install react-native-info
+```
+
+## Usage
+
+```js
+import { isFromAppStore } from 'react-native-info';
+
+// ...
+
+const result = await isFromAppStore();
+```
+
+## Contributing
+
+See the [contributing guide](CONTRIBUTING.md) to learn how to contribute to the repository and the development workflow.
+
+## License
+
+MIT
+
+---
+
+Made with [create-react-native-library](https://github.com/callstack/react-native-builder-bob)
---
+++ ios/DeviceInfo.mm
@@ -0,0 +1,29 @@
+#import "RNAppInstallationChecker.h"
+#import <React/RCTLog.h>
+#import <UIKit/UIKit.h>
+
+@implementation RNAppInstallationChecker
+
+RCT_EXPORT_MODULE();
+
+RCT_EXPORT_METHOD(isFromAppStore:(RCTPromiseResolveBlock)resolve rejecter:(RCTPromiseRejectBlock)reject) {
+    NSURL *receiptURL = [[NSBundle mainBundle] appStoreReceiptURL];
+    NSString *receiptPath = receiptURL.path;
+
+    if ([[NSFileManager defaultManager] fileExistsAtPath:receiptPath]) {
+        // Check if the app is installed from the App Store
+        NSDictionary *receiptDictionary = [NSDictionary dictionaryWithContentsOfFile:receiptPath];
+        NSString *receiptType = receiptDictionary[@"receipt_type"];
+
+        if ([receiptType isEqualToString:@"Production"]) {
+            resolve(@YES); // App is from App Store
+        } else {
+            resolve(@NO); // App is from TestFlight
+        }
+    } else {
+        // If the receipt doesn't exist, it's not from the App Store or TestFlight
+        resolve(@NO);
+    }
+}
+
+@end
